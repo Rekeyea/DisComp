3d6b22ce21af46b83a3a55ebbda5bcb61d14319d
Cristiano <cristianocca@hotmail.com>
2015-07-09 21:58:31 -0300

    correccion lista

--- backend/src/com/pyjava/core/PyList.java
+++ backend/src/com/pyjava/core/PyList.java
@@ -47,11 +47,17 @@ public class PyList extends PyObject {
         if(this.lista.size() > 0) {
             StringBuilder res = new StringBuilder("[");
             for (PyObject e : this.lista) {
-                res.append(e.__repr__().value + ", ");
+                if(e != this) {
+                    res.append(e.__repr__().value + ", ");
+                }
+                //Para evitar una recursion infinita.
+                else {
+                    res.append("[...], ");
+                }
             }
 
             res.deleteCharAt(res.length() - 1);
-            res.deleteCharAt(res.length() - 2);
+            res.deleteCharAt(res.length() - 1);
 
             res.append("]");
             return new PyString(res.toString());
