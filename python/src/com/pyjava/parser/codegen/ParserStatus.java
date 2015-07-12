3dc4e67cc8510f86e977bd661d5947c87ca83b5d
Emiliano Conti <econtirodriguez@outlook.com>
2015-07-11 21:28:29 -0300

    ParserStatus

---
+++ python/src/com/pyjava/parser/codegen/ParserStatus.java
@@ -0,0 +1,14 @@
+package com.pyjava.parser.codegen;
+
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.Deque;
+import java.util.Iterator;
+
+/**
+ * Created by rekeyea on 7/11/15.
+ */
+public class ParserStatus {
+    public static Deque<Generador> StackGenerador = new ArrayDeque<Generador>();
+
+}
